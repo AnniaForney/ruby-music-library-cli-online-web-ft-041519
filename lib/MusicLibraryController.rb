043c2469e2198ba2164e926929619762308e8f25
Annia Forney <anniaforney@gmail.com>
2019-05-19 06:35:13 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -83,7 +83,7 @@ end
     playlist = Song.all.sort{|a,b| a.name <=> b.name}
     if(1..playlist.length).include?(track)
     song = playlist[track+1]
-    puts "Playing #{song.name} by #{song.artist.name}"
+    puts "Playing #{track.name} by #{track.artist.name}"
   end
 end
 
